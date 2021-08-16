d92912f88b3751eb3f97fa02d5557e1ee9f80202
Juan Esteban <juanestban000@gmail.com>
2021-08-15 20:16:21 -0500

    Feat: fix babel-eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,19 @@
 module.exports = {
   env: {
     commonjs: true,
+    browser: true,
     es2021: true,
     node: true,
   },
   extends: ['standard', 'prettier'],
+  parser: 'babel-eslint',
   parserOptions: {
+    // ecmaVersion: 6,
     ecmaVersion: 12,
   },
-  rules: {},
+  rules: {
+    'comma-dangle': 0,
+    'import/first': 0,
+    'space-before-function-paren': 0,
+  },
 }
