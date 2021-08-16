26bb164585bcd6dc4d270fad58ea3d7353a5bfca
Juan Esteban <juanestban000@gmail.com>
2021-08-16 10:36:45 -0500

    Refactor: the sentence object without the switcher, change for any object that control the error names

--- src/middlewares/handleErrors.js
+++ src/middlewares/handleErrors.js
@@ -1,13 +1,23 @@
+const HANDLE_ERRORS = {
+  CastError: (res, { message }) =>
+    res
+      .status(400)
+      .json({ message: 'id used is malformed', stackTrace: message })
+      .end(),
+  ValidationError: (res, { message }) =>
+    res
+      .status(409)
+      .json({ message: 'id used is malformed', stackTrace: message })
+      .end(),
+  JsonWebTokenError: (res) =>
+    res.status(401).json({ message: 'token invalid or missing' }).end(),
+  defaultError: (res, error) => res.status(500).json(error).end(),
+}
+
 const handlerErrors = (error, req, res, next) => {
   console.log(error.name)
-  console.error(error.message)
-  if (error.name === 'CastError') {
-    return res
-      .status(400)
-      .json({ message: 'id used is malformed', stackTrace: error.message })
-      .end()
-  }
-  return res.status(500).end()
+  const handler = HANDLE_ERRORS[error.name] ?? HANDLE_ERRORS.defaultError
+  return handler(res, error)
 }
 
 module.exports = handlerErrors
