722d9a5e05867b6818766dea40cfa85120aa8385
Juan Esteban <juanestban000@gmail.com>
2021-08-15 20:19:09 -0500

    Feat: creating the controller principal and more important

---
+++ src/controllers/PrimaryController.js
@@ -0,0 +1,67 @@
+class PrimaryController {
+  constructor(SchemeMongo) {
+    this.SchemeMongo = SchemeMongo
+  }
+
+  findAll = async (_, res, next) => {
+    try {
+      const arraysObjects = await this.SchemeMongo.find()
+      return res.status(200).json(arraysObjects).end()
+    } catch (e) {
+      next(e)
+    }
+  }
+
+  findById = async (req, res, next) => {
+    try {
+      const { id } = req.params
+      const object = await this.SchemeMongo.findById(id)
+
+      if (object) return res.status(200).json(object).end()
+      return res.status(404).json({ message: "this id hasn't exist" }).end()
+    } catch (e) {
+      next(e)
+    }
+  }
+
+  create = async (req, res, next) => {
+    try {
+      const { body } = req
+      const createdObject = new this.SchemeMongo(body)
+      const savedObject = await createdObject.save()
+
+      return res.status(200).json(savedObject).end()
+    } catch (e) {
+      next(e)
+    }
+  }
+
+  update = async (req, res, next) => {
+    try {
+      const { body, params } = req
+      const { id } = params
+      const updatedObject = await this.SchemeMongo.findByIdAndUpdate(id, body, {
+        new: true,
+      })
+
+      if (updatedObject) return res.status(200).json(updatedObject).end()
+      return res.status(404).json({ message: "this id hasn't exist" }).end()
+    } catch (e) {
+      next(e)
+    }
+  }
+
+  delete = async (req, res, next) => {
+    try {
+      const { id } = req.params
+      const deletedObject = await this.SchemeMongo.findByIdAndDelete(id)
+
+      if (deletedObject) return res.status(204).json(deletedObject).end()
+      return res.status(404).json({ message: "this is hasn't exist" }).end()
+    } catch (e) {
+      next(e)
+    }
+  }
+}
+
+module.exports = PrimaryController
